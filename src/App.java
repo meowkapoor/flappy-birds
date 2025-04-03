fb1c257ec39f28900d5c8ee98e04382f7e4fd622
meowkapoor <2205885@kiit.ac.in>
2025-04-03 19:22:40 +0530

    frame set up(initial)

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardwidth = 360;
+        int boardheight = 640;
+
+        JFrame frame = new JFrame("flappy birds");
+        frame.setVisible(true);
+        frame.setSize(boardwidth, boardheight);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
