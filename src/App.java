250b21068a8266bb40d2adffff4407516b038f5a
meowkapoor <2205885@kiit.ac.in>
2025-04-07 14:34:39 +0530

    bird and background set

--- src/App.java
+++ src/App.java
@@ -9,9 +9,6 @@ public class App {
         // frame.setVisible(true);
 
         frame.setSize(boardwidth, boardheight);
-        // Directly sets the window size.
-        // Tells the OS: Make this window width x height pixels.
-
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,15 +16,6 @@ public class App {
         FlappyBirds flappyBirds = new FlappyBirds();
         frame.add(flappyBirds);
         frame.pack();
-        // If you don’t use pack(), then the preferred size of components (like JPanel)
-        // is ignored, unless:
-        // You manually call setSize() on the frame.
-        // Or you manually set size on each component and manage layout yourself.
-        // The window might open very small or default size, and your panel may not even
-        // show fully.
-        // Because the frame has no idea how much space it should allocate for its
-        // contents.
-
         frame.setVisible(true);
 
     }
--- src/FlappyBirds.java
+++ src/FlappyBirds.java
@@ -8,12 +8,54 @@ public class FlappyBirds extends JPanel {
     int boardwidth = 360;
     int boardheight = 640;
 
+    // IMAGES
+    Image backgroundImg;
+    Image bottomPipeImg;
+    Image topPipeImg;
+    Image birdImg;
+
+    int birdX = boardwidth / 8;
+    int birdY = boardheight / 2;
+    int birdWidth = 34;
+    int birdHeihgt = 24;
+
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int w = birdWidth;
+        int h = birdHeihgt;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    Bird bird;
+
     FlappyBirds() {
         setPreferredSize(new Dimension(boardwidth, boardheight));
-        // Tells the layout manager how big this panel wants to be.
-        // It doesn’t force the size — it suggests it.
-        // The layout manager may ignore it, if it wants to.
-
         setBackground(Color.blue);
+
+        // LOAD IMAGES
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+        bird = new Bird(birdImg);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // baclground
+        g.drawImage(backgroundImg, 0, 0, boardwidth, boardheight, null);
+
+        // bird
+        g.drawImage(birdImg, bird.x, bird.y, bird.w, bird.h, null);
     }
 }
